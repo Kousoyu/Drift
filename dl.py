4fcab9723f57315c665950742ffa6a5a76ce31f4
Kousoyu <770668004@qq.com>
2026-04-08 20:21:40 +0800

    chore: 清理工作区-删除16个测试/临时文件+完善gitignore

--- dl.py
+++
@@ -1,9 +0,0 @@
-import re
-html = open('baozi_img.html', 'r', encoding='utf-8').read()
-match = re.search(r'data-astro-cid-[\w]+>(.*?)<\/script>', html)
-if match:
-    print('FOUND SCRIPT:', match.group(1)[:200])
-
-for s in re.findall(r'<script.*?>.*?</script>', html, re.DOTALL):
-    if '1782702' in s or '32973' in s:
-        print('SCRIPT WITH IDS:', s[:200])
